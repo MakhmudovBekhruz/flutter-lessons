40cdabaf8779be8378ec3ea9c669293fe9f10c4e
Bekhruz <makhmudovbekhruzish@gmail.com>
2021-09-05 14:11:57 +0500

    Section 2.7

--- Section 2/s_2_7_stack_va_container/lib/main.dart
+++ Section 2/s_2_7_stack_va_container/lib/main.dart
@@ -5,7 +5,31 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Image.asset('assets/spiderman.png'),
+          child: Stack(
+            alignment: Alignment.topRight,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.red,
+                    width: 2,
+                  ),
+                ),
+                child: Text(
+                  "Spider",
+                  style: TextStyle(
+                    fontSize: 70,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Image.asset(
+                'assets/spiderman.png',
+                width: 70,
+              ),
+            ],
+          ),
         ),
       ),
     ),
